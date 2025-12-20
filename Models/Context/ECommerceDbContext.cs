61d12e4f86e2fcac297aed2e7744ff1251ca7463
fumihiko.kariya <fumihiko_kariya@icloud.com>
2025-12-20 11:33:09 +0900

    Initial Commit

---
+++ Models/Context/ECommerceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleECommerce.Models.Context
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        { 
+        
+        }
+    }
+}
