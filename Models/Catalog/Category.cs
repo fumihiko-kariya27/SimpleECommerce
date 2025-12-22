fa5334fb82515e67405f9e7cab250e27f030b3ff
fumihiko.kariya <fumihiko_kariya@icloud.com>
2025-12-22 21:26:37 +0900

    商品クラス追加

---
+++ Models/Catalog/Category.cs
@@ -0,0 +1,16 @@
+﻿using SimpleECommerce.Domain.Catalog.Categories;
+using SimpleECommerce.Models.Catalog;
+
+namespace SimpleECommerce.Models.Catalog
+{
+    public class Category
+    {
+        public CategoryId Id { get; set; } = CategoryId.None;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<Product> Products { get; } = new List<Product>();
+    }
+}
