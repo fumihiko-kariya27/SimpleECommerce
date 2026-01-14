8958a14605a620e7694c8ba587f39a7a629b3c0d
fumihiko.kariya <fumihiko_kariya@icloud.com>
2026-01-14 22:54:14 +0900

    商品削除機能追加

---
+++ wwwroot/js/modal.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener("DOMContentLoaded", event => {
+    const modal = document.getElementById("modal");
+    const modalMessage = document.getElementById("modal-message");
+    const yesButton = document.getElementById("modal-yes");
+    const noButton = document.getElementById("modal-no");
+
+    let deleteUrl = null;
+
+    document.querySelectorAll(".delete-link").forEach(link => {
+        link.addEventListener("click", e => {
+            e.preventDefault();
+
+            const name = link.dataset.name;
+            deleteUrl = link.dataset.url;
+            modalMessage.innerText = `${name}を削除します。この操作は元に戻せませんが、よろしいですか？`;
+            modal.style.display = "flex";
+
+            // モーダルを開いている時はスクロール不可とする
+            document.body.style.overflow = "hidden";
+        })
+    });
+
+    yesButton.addEventListener("click", e => {
+        if (deleteUrl) {
+            window.location.href = deleteUrl;
+        }
+    });
+
+    noButton.addEventListener("click", e => {
+        modal.style.display = "none";
+
+        document.body.style.overflow = "auto";
+    });
+
+    // ESCキーで閉じられるように設定しておく
+    document.addEventListener("keydown", event => {
+        if (event.key === "Escape" && modal.style.display === "flex") {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto";
+        }
+    });
+});
