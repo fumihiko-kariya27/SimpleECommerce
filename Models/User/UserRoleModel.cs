ee3c472aba70393878b8c150b3ab1adbdda2a3d6
fumihiko.kariya <fumihiko_kariya@icloud.com>
2026-01-18 12:00:07 +0900

    認証関連モデルの追加

---
+++ Models/User/UserRoleModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleECommerce.Models.User.Authorization;
+
+namespace SimpleECommerce.Models.User
+{
+    [PrimaryKey(nameof(UserId), nameof(RoleId))]
+    public class UserRoleModel
+    {
+        public int UserId { get; set; }
+
+        public UserModel User { get; set; } = null!;
+
+        public int RoleId { get; set; }
+
+        public RoleModel Role { get; set; } = null!;
+    }
+}
