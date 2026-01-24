bb536e3a24ef7b0e6dff4434fff5108fbe7a31f7
fumihiko.kariya <fumihiko_kariya@icloud.com>
2026-01-24 10:48:05 +0900

    認証設定拡張クラスの追加

---
+++ Controllers/Auth/Ext/AuthExtension.cs
@@ -0,0 +1,55 @@
+﻿namespace SimpleECommerce.Controllers.Auth.Ext
+{
+    public static class AuthExtension
+    {
+        public static IServiceCollection AddPermissionPolicies(this IServiceCollection services)
+        {
+            services.AddAuthorizationBuilder()
+                .AddPolicy("ViewProduct", policy =>
+                {
+                    policy.RequireClaim("Permission", "ViewProduct");
+                })
+                .AddPolicy("RegisterNewProduct", policy =>
+                {
+                    policy.RequireClaim("Permission", "RegisterNewProduct");
+                })
+                .AddPolicy("UpdateProduct", policy =>
+                {
+                    policy.RequireClaim("Permission", "UpdateProduct");
+                })
+                .AddPolicy("DeleteProduct", policy =>
+                {
+                    policy.RequireClaim("Permission", "DeleteProduct");
+                })
+                .AddPolicy("NewOrder", policy =>
+                {
+                    policy.RequireClaim("Permission", "NewOrder");
+                })
+                .AddPolicy("UpdateOrder", policy =>
+                {
+                    policy.RequireClaim("Permission", "UpdateOrder");
+                })
+                .AddPolicy("CancelOrder", policy =>
+                {
+                    policy.RequireClaim("Permission", "CancelOrder");
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCookeiAythentication(this IServiceCollection services)
+        {
+            services.AddAuthentication("AuthCookie").AddCookie(
+                "AuthCookie", options =>
+                {
+                    options.LoginPath = "/auth/login";
+                    options.LogoutPath = "/auth/logout";
+                    options.AccessDeniedPath = "/auth/denied";
+                    options.ExpireTimeSpan = TimeSpan.FromHours(3);
+                }
+            );
+
+            return services;
+        }
+    }
+}
